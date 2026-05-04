fe5294415eb4eb386eb22333c36f3e800371dc07
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-04 11:42:10 -0300

    (refactor) migrando funções de disassembler para display_code.c

---
+++ src/display_code.c
@@ -0,0 +1,112 @@
+#include "display.h"
+#include "bytecode.h"
+
+void print_operands(const ClassFile* cf, Reader *code_reader,
+    u1 opc, FILE* out, int indent) {
+  const opcode_info* opi = &opcode_table[opc];
+  u4 base_pc = code_reader->pos - 1;
+  if (!opi->operands) return;
+
+  if (opi->type == OP_CP) {
+    u2 idx = opi->operands == 2 ? read_u2(code_reader) :
+      read_u1(code_reader);
+    fprintf(out, "#%d // ", idx);
+    print_cp_value(idx, cf, out);
+  }
+
+  if (opi->type == OP_LOCAL || opi->type == OP_LITERAL) {
+    if (opi->operands == 2) {
+      fprintf(out, "%d", (int16_t)read_u2(code_reader));
+    } else {
+      fprintf(out, "%d", (int8_t)read_u1(code_reader));
+    }
+  }
+
+  if (opi->type == OP_BRANCH) {
+    int32_t offset;
+    if (opi->operands == 2) {
+      offset = (int16_t)read_u2(code_reader);
+    } else if (opi->operands == 4) {
+      offset = (int32_t)read_u4(code_reader); 
+    } else {
+      offset = (int8_t)read_u1(code_reader);
+    }
+    fprintf(out, "%d", base_pc + offset);
+  }
+
+  if (opi->type == OP_SPECIAL) {
+    switch (opc) {
+      // tableswitch
+      case opc_tableswitch: {
+        while (code_reader->pos % 4 != 0) 
+          read_u1(code_reader); // 0 a 3 bytes de padding
+        u4 default_offset = read_u4(code_reader);
+        u4 low_byte = read_u4(code_reader);
+        u4 high_byte = read_u4(code_reader);
+
+        // tabela de salto
+        fputc('\n', out);
+        print_indent(indent, out);
+        fprintf(out, "(%d to %d)\n", low_byte, high_byte);
+        for (u4 i = 0; i <= high_byte - low_byte; i++) {
+          print_indent(indent, out);
+          fprintf(out, "%3d: %-3d\n", low_byte + i, base_pc + read_u4(code_reader));
+        }
+
+        print_indent(indent, out);
+        fprintf(out, "default: %-3d", default_offset + base_pc);
+        break;
+      }
+
+      case opc_lookupswitch:
+        while (code_reader->pos % 4 != 0) 
+          read_u1(code_reader); // 0 a 3 bytes de padding
+
+        int32_t default_offset = (int32_t)read_u4(code_reader);
+        int32_t npairs = (int32_t)read_u4(code_reader);
+        
+        fputc('\n', out);
+        print_indent(indent, out);
+        fprintf(out, "(%d pairs) \n", npairs);
+        
+        while (npairs--) {
+          int32_t match = (int32_t)read_u4(code_reader);
+          int32_t offset = (int32_t)read_u4(code_reader);
+        
+          print_indent(indent, out);
+          fprintf(out, "%4d: %d\n", (int32_t)match, (int32_t)offset + base_pc);
+        }
+        
+        print_indent(indent, out);
+        fprintf(out, "default: %d", default_offset + base_pc);
+        break;
+        
+      default:
+        break;
+
+    }
+  }
+} 
+
+void print_code(const ClassFile *cf, 
+    const Code_attribute* code, FILE* out, int indent) {
+  print_indent(indent, out);
+  fprintf(out, "max_stack: %d, max_locals: %d\n", code->max_stack,
+      code->max_locals);
+
+  Reader code_reader = { code->code, code->code_length, 0 };
+  
+  // TODO operandos
+  while (code_reader.pos < code_reader.size) {
+    print_indent(indent, out);
+    u4 pc = code_reader.pos;
+    u1 opc = read_u1(&code_reader);
+    fprintf(out, "%3d: %-15s ", pc, opcode_table[opc].name);
+
+    print_operands(cf, &code_reader, opc, out, indent+5);
+    fputc('\n', out);
+  }
+
+
+  return;
+}
