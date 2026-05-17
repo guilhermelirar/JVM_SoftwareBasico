b109de0f70e20c7cbfe4ab57097267f338662315
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-17 14:39:47 -0300

    (jvm) wait

--- src/jvm/interpreter.c
+++ src/jvm/interpreter.c
@@ -1,6 +1,8 @@
 #include "jvm/interpreter.h"
+#include "common/classfile.h"
 #include "jvm/jvmtypes.h"
 #include "common/bytecode.h"
+#include <string.h>
 
 static inline u1 fetch_u1(u1 *code, u4 *pc) {
   return code[(*pc)++];
@@ -31,6 +33,14 @@ void handle_getstatic(JVM_Context *ctx)
 {
   Frame* frame = ctx->t.frames[ctx->t.frame_ptr];
   u2 cp_idx = fetch_u2(frame->code, &frame->pc);
+
+  // encontrando cpinfo
+  cp_info* cp_entry = &frame->constant_pool[cp_idx];
+  
+  // TODO lidar melhor
+  if (cp_entry->tag != CONSTANT_Fieldref) return;
+  u2 class_index = cp_entry->info.fieldref_info.class_index;
+  u2 nt_index = cp_entry->info.fieldref_info.name_and_type_index;
 }
 
 const instruction_handler DISPATCH_TABLE[256] = {
