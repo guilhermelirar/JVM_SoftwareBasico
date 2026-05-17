570b19cfbaa2b1a4389f0cd79a1eff806f8c9251
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-17 11:11:34 -0300

    (jvm) documetando primeiras funções

--- include/jvm/jvm.h
+++ include/jvm/jvm.h
@@ -4,9 +4,23 @@
 #include "common/classfile.h"
 #include "jvmtypes.h"
 
+/**
+ * @brief retorna method_info de um método de nome name e descritor fornecido
+ * @param cf ClassFile que contém o método
+ * @param name nome simples do método
+ * @param descriptor descritor de tipos do método
+ * @return method_info do método encontrado, ou NULL caso o método 
+ * não tenha sido encontrado
+ */
 method_info* find_method(ClassFile *cf, const char* name, 
     const char* descriptor);
+/**
+ * @brief inicializa JVM_Context ao receber classe de entrada
+ * @param main_class ClassFile inicializada que deve ser ponto de entrada 
+ * da execução
+ * @return JVM_Context pronto para loop de execução caso main_class seja válida,
+ * ou NULL caso main_class seja inválida (ex: sem método main correto)
+ */
 JVM_Context* jvm_init(ClassFile* main_class);
-
 
 #endif
