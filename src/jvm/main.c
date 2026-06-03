e603121e7c84f321ec75682c5db6bcf0c35452b6
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-06-02 21:30:08 -0300

    (jvm main) removendo leitura desnecessaria de classfile

--- src/jvm/main.c
+++ src/jvm/main.c
@@ -1,5 +1,3 @@
-#include "common/classfile.h"
-#include "common/classfile_reader.h"
 #include "jvm/jvm.h"
 #include "jvm/jvmtypes.h"
 #include "jvm/utils.h"
@@ -8,7 +6,6 @@ int main(int argc, char** argv)
 {
   if (argc < 2) return 1;
 
-  ClassFile* main_class = ClassFile_from_path(argv[1]);
 
   JVM_Context* ctx = jvm_init();
   extract_class_dir(argv[1], ctx->base_dir, 256);
@@ -16,7 +13,6 @@ int main(int argc, char** argv)
 
   jvm_run(ctx);
 
-  free_classfile(main_class);
   terminateJVM(ctx);
   return 0;
 }
