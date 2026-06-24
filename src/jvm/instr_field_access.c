6bd5297ce51b0c814fb9316ecc9116a68f75e2e5
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-06-24 11:18:14 -0300

    (jvm) getfield implementado

--- src/jvm/instr_field_access.c
+++ src/jvm/instr_field_access.c
@@ -83,6 +83,55 @@ void handle_putstatic(JVM_Context* ctx, u1 opc)
   field->holder_class->static_fields[field->index] = pop_operand(frame); 
 }
 
+
+void handle_getfield(JVM_Context *ctx, u1 opc)
+{
+  (void)opc;
+  Frame* frame = current_frame(ctx);
+  u2 cp_idx = fetch_u2(&frame->pc);
+
+  RuntimeField* field = resolve_field(ctx, cp_idx, false);
+
+  bool is_cat2 = field->descriptor[0] == 'J' || field->descriptor[0] == 'D';
+
+  u4 obj_ref = pop_operand(frame);
+  if (!obj_ref) goto null_ptr;
+  Object* obj = &ctx->objects.entries[obj_ref];
+
+  // se protegido, e resolução aconteceu, classe de obj 
+  // deve ser subclasse da classe atual ou classe atual
+  if ((field->access_flags & ACC_PROTECTED) && 
+      !extends(obj->clazz, frame->method.holder_class))
+    goto illegal_access;
+
+  // mesma coisa se campo privado e classe não é atual 
+  if ((field->access_flags & ACC_PRIVATE) && 
+      frame->method.holder_class != field->holder_class)
+    goto illegal_access;
+
+  if (is_cat2)
+  {
+    u8 val = obj->content.fields[field->index];
+    val <<= 32;
+    val |= 0xFF & obj->content.fields[field->index + 1];
+    push_operand2(frame, val);
+    return;
+  }
+
+  push_operand(frame, obj->content.fields[field->index]);
+  return;
+
+// TODO throw
+illegal_access:
+  terminateJVM(ctx);
+  fprintf(stderr, "IllegalAccessError");
+  exit(1);
+null_ptr: 
+  terminateJVM(ctx);
+  fprintf(stderr, "NullPointerException");
+  exit(1);
+}
+
 void handle_putfield(JVM_Context* ctx, u1 opc)
 {
   (void)opc;
